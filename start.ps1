64988fcefb9f70808fa3d3174f22c1842cdc0c53
Jenkins <jenkins@example.com>
2025-01-07 08:10:08 +0100

    add operators manifest files

---
+++ start.ps1
@@ -0,0 +1,25 @@
+# Liste des fichiers .yml à appliquer
+$yamlFiles = @(
+    "argocd-init.yml",
+    "postgres-application.yml",
+     "discovery-application.yml",
+     "gateway-application.yml",
+    "user-application.yml",
+    "facture-application.yml",
+    "payment-transaction-application.yml"
+
+)
+
+# Boucle pour appliquer chaque fichier .yml avec kubectl
+foreach ($yamlFile in $yamlFiles) {
+    if (Test-Path $yamlFile) {
+        Write-Host "Appliquons le fichier: $yamlFile"
+        kubectl apply -f $yamlFile
+    } else {
+        Write-Host "Le fichier $yamlFile n'existe pas."
+    }
+}
+
+# Affichage de l'état des ressources déployées
+Write-Host "Vérification de l'état des pods, services et autres ressources..."
+kubectl get all
